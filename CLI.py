791c0b4efdb8c99bb733f1273b54503e41f69eba
Iris <irisbregu20@gmail.com>
2024-11-11 16:15:12 +0100

    Change theme, add clock, handle edit and complete error

--- CLI.py
+++ CLI.py
@@ -1,34 +1,41 @@
 import function
+import time
+
+now=time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
 while True:
-    user_action=input("Type add, show, edit, complete, exit:")
+    user_action = input("Type add, show, edit, complete, exit: ")
     user_action=user_action.strip()
-    
-    if user_action.startswith('add'):
-        todo=user_action[4:]
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
 
         todos=function.get_todos()
 
-        todos.append(todo+'\n')
+        todos.append(todo + '\n')
 
         function.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos=function.get_todos("todos.txt")
+        todos = function.get_todos("todos_item.txt")
 
         for index, item in enumerate(todos):
-            item=item.strip('\n')
-            row=f"{index+1}-{item}"
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
             print(row)
 
     elif user_action.startswith('edit'):
         try:
-            number=int(user_action[5:])
+            number = int(user_action[5:])
+            print(number)
+
             number=number-1
 
-            todos=function.get_todos()
+            todos = function.get_todos()
 
-            new_todo=input("Vendos todo e re: ")
-            todos[number]=new_todo+'\n'
+            new_todo = input("Vendos todo e re: ")
+            todos[number] = new_todo + '\n'
 
             function.write_todos(todos)
         except ValueError:
@@ -37,26 +44,28 @@ while True:
 
     elif user_action.startswith('complete'):
         try:
-            number=int(user_action[9:])
+            number = int(user_action[9:])
 
-            todos=function.get_todos()
-
+            todos = function.get_todos()
             index=number-1
-            todo_to_remove=todos[index].strip('\n')
+            todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
             function.write_todos(todos)
-            message=f"Todo {todo_to_remove} was removed from the list"
+
+            message=f"Todo {todo_to_remove} was removed from the list."
             print(message)
         except IndexError:
-            print("There is no item with that number!")
+            print("Komanda e vendosur nuk eshte e vlefshme!")
             continue
 
     elif user_action.startswith('exit'):
         break
     else:
-        print("Komanda e vendosur nuk eshte e vlefshme! ")
-print("Byeeeee!")
+        print("Komanda e vendosur nuk eshte e vlefshme!")
+
+print("Bye!")
+
 
 
 
